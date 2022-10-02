4d87de6548e22142068eb384eab30ac48e963782
PavelStoyanov06 <114023028+PavelStoyanov06@users.noreply.github.com>
2022-10-02 10:56:12 +0300

    add an openable link prompt at the end of the program

--- GuesTheNumber/GuesTheNumber/Program.cs
+++ GuesTheNumber/GuesTheNumber/Program.cs
@@ -1,18 +1,85 @@
 ﻿using Pastel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+static void OpenUrl(string url)
+{
+    try
+    {
+        Process.Start(url);
+    }
+    catch
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            url = url.Replace("&", "^&");
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", url);
+        }
+        else
+        {
+            throw;
+        }
+    }
+}
 
 static void NewGame()
 {
-    Random randNum = new Random();
-    int selectedNum = randNum.Next(1, 101);
+    Console.Title = "Guess The Number";
+    string[] diffs = { "easy", "medium", "hard", "extreme", "custom" };
+    Console.WriteLine("Choose between: " + "easy".Pastel("#55FF55") + " medium".Pastel("#00AA00") +"[default]" + " hard".Pastel("#FFAA00") + " extreme".Pastel("#AA0000") + " custom".Pastel("#FF55FF"));
+    Console.Write("Enter the games difficulty:".Pastel("#5555FF"));
+    string diffInput = Console.ReadLine();
 
-    Console.WriteLine("Please enter an integer between 1 and 100!".Pastel("#FF5555"));
+    while (!diffs.Contains(diffInput.ToLower()))
+    {
+        Console.WriteLine("Invalid input!".Pastel("#AA0000"));
+        Console.Write("Enter the games difficulty:".Pastel("#5555FF"));
+        diffInput = Console.ReadLine();
+    }
+
+    int endNum = 100;
+
+    switch (diffInput)
+    {
+        case "easy":
+            endNum = 50;
+            break;
+        case "medium":
+            endNum = 100;
+            break;
+        case "hard":
+            endNum = 1000;
+            break;
+        case "extreme":
+            endNum = 10000;
+            break;
+        case "custom":
+            Console.Write("Please enter a number:".Pastel("#AA00AA"));
+            endNum = int.Parse(Console.ReadLine());
+            break;
+    }
+
+    Console.WriteLine();
+
+    Random randNum = new();
+    int selectedNum = randNum.Next(1, endNum + 1);
+
+    Console.WriteLine($"Please enter an integer between 1 and {endNum}!".Pastel("#FF5555"));
 
     while (true)
     {
         Console.Write("Enter a number:");
         string userInput = Console.ReadLine();
         bool isValid = int.TryParse(userInput, out int userNum);
-        if(userNum > 100 || userNum < 0) isValid = false;
+        if(userNum > endNum || userNum < 0) isValid = false;
 
 
         if (isValid)
@@ -50,7 +117,14 @@ static void NewGame()
         }
     }
 
+    Console.WriteLine();
     Console.WriteLine("Hope you enjoyed the game!".Pastel("#FFAA00"));
+    Console.Write("Press [O] if you want to visit me on githib or press [ENTER] to finish the program!".Pastel("#FFFFFF"));
+
+    if (Console.ReadKey().Key == ConsoleKey.O)
+    {
+        OpenUrl("https://github.com/PavelStoyanov06/Guess-The-Number");
+    }
 }
 
 NewGame();
